7b1e11605a9fb5de0e17d52d36e4770c81eeab2a
Gabriel Santana <gabrielsantana77@hotmail.com>
2021-03-02 17:09:44 -0300

    desafio

---
+++ src/shared/infra/typeorm/migrations/1614714561841-CreateOrders.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateOrders1614714561841 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}

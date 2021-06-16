aedc477c4c83aa17d2a9d123299934c5184bd467
Gabriel Santana <gabrielsantana77@hotmail.com>
2021-06-16 20:34:53 -0300

    a

--- src/shared/infra/typeorm/migrations/1623089735505-CreateOrders.ts
+++ src/shared/infra/typeorm/migrations/1623089735505-CreateOrders.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateCustomers1614713967899
-  implements MigrationInterface {
+export default class CreateOrders1623089735505 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'customers',
+        name: 'orders',
         columns: [
           {
             name: 'id',
@@ -13,15 +12,6 @@ export default class CreateCustomers1614713967899
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
-          },
-          {
-            name: 'name',
-            type: 'varchar',
-          },
-          {
-            name: 'email',
-            type: 'varchar',
-            isUnique: true,
           },
           {
             name: 'created_at',
@@ -39,6 +29,6 @@ export default class CreateCustomers1614713967899
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('customers');
+    await queryRunner.dropTable('orders');
   }
 }
